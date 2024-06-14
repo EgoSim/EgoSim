7e7728765432e406ac6742f1d6e2079d90990ce3
Songheng Yin <yinsongheng@gmail.com>
2024-06-15 04:53:44 +0800

    update

--- static/js/video-selection.js
+++ static/js/video-selection.js
@@ -1,27 +1,30 @@
 const buttonGroupIds = ["drawerButtons", "washHandButtons", "kitchenButtons"];
-
+const posters = [
+    "videos/interactive/drawer_1stframe.png",
+    "videos/interactive/washhand_1stframe.png",
+    "videos/interactive/choppingboard_1stframe.png"
+];
 
 const showButtons = (idx) => {
     buttonGroupIds.forEach((x) => $(`#${x}`).hide());
     $(`#${buttonGroupIds[idx]}`).show();
 };
 
+const groupSelectionButtonHandler = (idx) => {
+    showButtons(idx);
+    $('#videoSource').attr('src', '');
+    $('#videoPlayer')[0].load();
+    $('#videoPlayer').attr('poster', posters[idx]);
+}
 
-// const buttonKitchenHandler = (e) => {
-//     showButtons(0);
-// };
-
-const changeVideoSource = (source, componentId="videoSource") => {
-    $(`#demoVideo`).attr('src', source);
+const changeVideoSource = (source) => {
+    // $(`#demoVideo`).attr('src', source);
+    $('#videoSource').attr('src', source);
+    $('#videoPlayer')[0].load();
+    $('#videoPlayer')[0].play();
 };
 
 const changeVideoHandler = (source, prompt) => {
     changeVideoSource(source);
     $('#caption').text(`Prompt: ${prompt}`)
-};
-
-
-
-const hello = () => {
-    console.log("hello");
 };
