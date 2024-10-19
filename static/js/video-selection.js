b3b13e885e09bc60e77d65ddbf268c1cef442b93
ReinaKousaka <yinsongheng@gmail.com>
2024-10-19 20:48:11 +0800

    update sample trajectory

--- static/js/video-selection.js
+++ static/js/video-selection.js
@@ -30,7 +30,6 @@ const groupSelectionButtonHandler = (idx) => {
 }
 
 const changeVideoSource = (source) => {
-    // $(`#demoVideo`).attr('src', source);
     $('#videoSource').attr('src', source);
     $('#videoPlayer')[0].load();
     $('#videoPlayer')[0].play();
@@ -39,4 +38,46 @@ const changeVideoSource = (source) => {
 const changeVideoHandler = (source, prompt) => {
     changeVideoSource(source);
     $('#caption').text(`Prompt: ${prompt}`)
+};
+
+const changeGifHandler = (source) => {
+    $('#extrinsic-gif').attr('src', source + '?t=' + new Date().getTime());
+};
+
+const srcMap = {
+    '4-1': {
+        video_source: 'videos/interactive/s4/washhand-zoomin.mp4',
+        caption: 'a person standing in front of sink and washing hand',
+        gif_source: 'videos/extrinsics/s4/7-1.gif',
+    },
+    '4-2': {
+        video_source: 'videos/interactive/s4/washhand-zoomout.mp4',
+        caption:'a person standing in front of sink and washing hand',
+        gif_source: 'videos/extrinsics/s4/7-2.gif',
+    },
+    '4-3': {
+        video_source: 'videos/interactive/s4/washhand-up.mp4',
+        caption:'a person standing in front of sink and washing hand',
+        gif_source: 'videos/extrinsics/s4/7-3.gif',
+    },
+    '4-4': {
+        video_source: 'videos/interactive/s4/washhand-up-zoomout.mp4',
+        caption:'a person standing in front of sink and washing hand',
+        gif_source: 'videos/extrinsics/s4/7-4.gif',
+    },
+    '4-5': {
+        video_source: 'videos/interactive/s4/washhand_roll.mp4',
+        caption:'a person standing in front of sink and washing hand',
+        gif_source: 'videos/extrinsics/s4/7-5.gif',
+    },
+    '4-6': {
+        video_source: 'videos/interactive/s4/washhand_yaw.mp4',
+        caption:'a person standing in front of sink and washing hand',
+        gif_source: 'videos/extrinsics/s4/7-6.gif',
+    },
+};
+
+const changeVideoAndGifHandler = (id) => {
+    changeVideoHandler(srcMap[id].video_source, srcMap[id].caption);
+    changeGifHandler(srcMap[id].gif_source);
 };
